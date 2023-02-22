00f5f713674d254b7d8776abe691c7b040f95691
Doeca <i@doeca.cc>
2023-02-22 22:01:53 +0800

    [feat] add logout notification

--- wy.js
+++ wy.js
@@ -1,22 +1,27 @@
 const axios = require('axios');
 const prefix = "https://netease.doeca.cc"
+const notifyUrl = "http://tun4.cquluna.top:3002/notifyLogout"
 let loadStatus = 1;
 
 const target = () => {
-    console.log("test");
     try {
         axios.get(`${prefix}/login/status`).then(resp => {
-            if (resp.data.data.code != 200) 
+            if (resp.data.data.code != 200)
                 throw 'Call Fail';
-            
-            if(resp.data.data.account.status == -10)
-            {
-                if(loadStatus == 1)
-                {
+
+            if (resp.data.data.account.status == -10) {
+                if (loadStatus == 1) {
                     //notify to scan login
+                    try {
+                        fetch(notifyUrl, { mode: "cors" }).then(res => { })
+                    } catch (error) {
+
+                    }
                     loadStatus = 0;
                     console.log("[Logout] 网易云登陆已失效，需要重新登陆")
                 }
+            } else {
+                loadStatus = 1;
             }
         })
     } catch (err) {
