f7ed43b7b78b7a4e43377094c78d91cb4d60370b
Doeca <i@doeca.cc>
2023-02-22 18:03:01 +0800

    change api url

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const router = require('koa-router')();
 const app = new Koa();
 const wp_musicapi = require("./wp_MusicApi");
-const domain = "https://musicApi.doeca.cc:20050"
+const domain = "http://musicapi.doeca.cc:3001"
 const wyApi = "https://netease.doeca.cc"
 
 
