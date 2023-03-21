a702c13f351f288dc92a66cf28ef50503f46f493
Doeca <i@doeca.cc>
2023-03-21 23:21:42 +0800

    [feat]:using vip song to check

--- wy.js
+++ wy.js
@@ -1,31 +1,17 @@
 const axios = require('axios');
 const prefix = "http://netease.doeca.cc"
 const notifyUrl = "http://tun4.cquluna.top:3002/notifyLogout"
+const notifyinUrl = "http://tun4.cquluna.top:3002/notifyLogin"
 let loadStatus = 1;
 
 const target = () => {
     try {
-        axios.get(`${prefix}/login/status`).then(resp => {
+        axios.get(`${prefix}/song/url?id=29713754`).then(resp => {
             if (resp.data.data.code != 200)
                 throw 'Call Fail';
 
-            if (resp.data.data.account == null) {
-                if (loadStatus == 1) {
-                    //notify to scan login
-                    try {
-                        fetch(notifyUrl, { mode: "cors" }).then(res => { })
-                    } catch (error) {
 
-                    }
-                    loadStatus = 0;
-                    console.log("[Logout] 网易云登陆已失效，需要重新登陆")
-                }
-                return;
-            } else {
-                loadStatus = 1;
-            }
-
-            if (resp.data.data.account.status != 0) {
+            if (resp.data.data.size != 24101355) {
                 if (loadStatus == 1) {
                     //notify to scan login
                     try {
@@ -37,7 +23,17 @@ const target = () => {
                     console.log("[Logout] 网易云登陆已失效，需要重新登陆")
                 }
             } else {
-                loadStatus = 1;
+                if(loadStatus == 0)
+                {
+                    try {
+                        fetch(notifyinUrl, { mode: "cors" }).then(res => { })
+                    } catch (error) {
+
+                    }
+                    loadStatus = 1;
+                    console.log("[Logout] 网易云登陆已恢复")
+                }
+                
             }
         })
     } catch (err) {
